9f2345f46811ecc296e60eb2377ae1b17009de88
Arif Hendrawan Priliyanto <55530939+arifhendrawan023@users.noreply.github.com>
2022-05-15 16:34:28 +0700

    Add files via upload

---
+++ webpack.prod.js
@@ -0,0 +1,59 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const { merge } = require('webpack-merge');
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: '/node_modules/',
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+      },
+      {
+        test: /.s?css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../../',
+            },
+          },
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      `...`,
+      new CssMinimizerPlugin({
+        minify: [
+          CssMinimizerPlugin.cssnanoMinify,
+          CssMinimizerPlugin.cssoMinify,
+          CssMinimizerPlugin.cleanCssMinify,
+        ],
+        minimizerOptions: {
+          preset: [
+            'advanced',
+          ],
+        },
+      }),
+    ],
+  },
+  plugins: [
+    new BundleAnalyzerPlugin(),
+    new MiniCssExtractPlugin({filename: 'assets/css/[hash].css'}),
+  ],
+});
